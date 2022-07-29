823845fbb0bd0f010a9b1ccd1d46815b55135c0e
Sagar <sagarbiophysics@gmail.com>
2022-07-29 14:36:28 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Multishot TOmography'
+copyright = '2022, Sagar Khavnekar'
+author = 'Sagar Khavnekar'
 
 release = '0.1'
 version = '0.1.0'
